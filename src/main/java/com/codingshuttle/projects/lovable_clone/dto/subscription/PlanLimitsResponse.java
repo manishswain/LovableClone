f17e676f769dcb332e2bd8e8bf356d210bdac742
manishswain <manishswain2121@gmail.com>
2026-01-15 15:20:48 +0530

    Controllers and Service Method Boilerplate Added

---
+++ src/main/java/com/codingshuttle/projects/lovable_clone/dto/subscription/PlanLimitsResponse.java
@@ -0,0 +1,9 @@
+package com.codingshuttle.projects.lovable_clone.dto.subscription;
+
+public record PlanLimitsResponse(
+        String planName,
+        int maxTokensPerDay,
+        int maxProjects,
+        boolean unlimitedAi
+) {
+}
