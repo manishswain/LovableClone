5e16645ab207b2cab63f7d9de339917e803f3f6e
manishswain <manishswain2121@gmail.com>
2026-01-17 12:57:24 +0530

    Project Member Service Layer Implemented

--- src/main/java/com/codingshuttle/projects/lovable_clone/dto/member/InviteMemberRequest.java
+++ src/main/java/com/codingshuttle/projects/lovable_clone/dto/member/InviteMemberRequest.java
@@ -4,6 +4,6 @@ import com.codingshuttle.projects.lovable_clone.enums.ProjectRole;
 
 public record InviteMemberRequest(
         String email,
-        ProjectRole role
+        ProjectRole projectRole
 ) {
 }
--- src/main/java/com/codingshuttle/projects/lovable_clone/service/impl/ProjectMemberServiceImpl.java
+++ src/main/java/com/codingshuttle/projects/lovable_clone/service/impl/ProjectMemberServiceImpl.java
@@ -3,30 +3,116 @@ package com.codingshuttle.projects.lovable_clone.service.impl;
 import com.codingshuttle.projects.lovable_clone.dto.member.InviteMemberRequest;
 import com.codingshuttle.projects.lovable_clone.dto.member.MemberResponse;
 import com.codingshuttle.projects.lovable_clone.dto.member.UpdateMemberRoleRequest;
+import com.codingshuttle.projects.lovable_clone.entity.Project;
+import com.codingshuttle.projects.lovable_clone.entity.ProjectMember;
+import com.codingshuttle.projects.lovable_clone.entity.ProjectMemberId;
+import com.codingshuttle.projects.lovable_clone.entity.User;
+import com.codingshuttle.projects.lovable_clone.mapper.ProjectMemberMapper;
+import com.codingshuttle.projects.lovable_clone.repository.ProjectMemberRepository;
+import com.codingshuttle.projects.lovable_clone.repository.ProjectRepository;
+import com.codingshuttle.projects.lovable_clone.repository.UserRepository;
 import com.codingshuttle.projects.lovable_clone.service.ProjectMemberService;
+import jakarta.transaction.Transactional;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+@Transactional
 public class ProjectMemberServiceImpl implements ProjectMemberService {
+
+    ProjectMemberRepository projectMemberRepository;
+    ProjectRepository projectRepository;
+    ProjectMemberMapper projectMemberMapper;
+    UserRepository userRepository;
+
     @Override
     public List<MemberResponse> getProjectMembers(Long projectId, Long userId) {
-        return List.of();
+        Project project = getAccessibleProjectByIdAndUserId(projectId, userId);
+        List<MemberResponse> memberResponseList = new ArrayList<>();
+        memberResponseList.add(projectMemberMapper.toProjectMemberResponse(project.getOwner()));
+
+        projectMemberRepository.findByIdProjectId(projectId)
+                .stream()
+                .map(projectMemberMapper::toProjectMemberResponseFromMember)
+                .forEach(memberResponseList::add);
+
+        return memberResponseList;
     }
 
     @Override
     public MemberResponse inviteMember(Long projectId, InviteMemberRequest request, Long userId) {
-        return null;
+        Project project = getAccessibleProjectByIdAndUserId(projectId, userId);
+
+        if(!project.getOwner().getId().equals(userId))
+            throw new RuntimeException("Only project owner can invite members");
+
+        User invitee = userRepository.findByEmail(request.email())
+                .orElseThrow(() -> new RuntimeException("User with email " + request.email() + " not found"));
+
+        if(invitee.getId().equals(userId))
+            throw new RuntimeException("Owner cannot be invited as member");
+
+        ProjectMemberId projectMemberId = new ProjectMemberId(projectId, invitee.getId());
+
+        if(projectMemberRepository.existsById(projectMemberId))
+            throw new RuntimeException("User is already a member of the project");
+
+        ProjectMember projectMember = ProjectMember.builder()
+                .id(projectMemberId)
+                .project(project)
+                .user(invitee)
+                .projectRole(request.projectRole())
+                .invitedAt(java.time.Instant.now())
+                .build();
+
+        projectMemberRepository.save(projectMember);
+
+        return projectMemberMapper.toProjectMemberResponseFromMember(projectMember);
     }
 
     @Override
     public MemberResponse updateMemberRole(Long projectId, Long memberId, UpdateMemberRoleRequest request, Long userId) {
-        return null;
+        Project project = getAccessibleProjectByIdAndUserId(projectId, userId);
+
+        if(!project.getOwner().getId().equals(userId))
+            throw new RuntimeException("Only project owner can update members role");
+
+        ProjectMemberId projectMemberId = new ProjectMemberId(projectId, memberId);
+
+        ProjectMember projectMember = projectMemberRepository.findById(projectMemberId)
+                .orElseThrow(() -> new RuntimeException("Member not found in the project"));
+
+        projectMember.setProjectRole(request.role());
+        projectMemberRepository.save(projectMember);
+
+        return projectMemberMapper.toProjectMemberResponseFromMember(projectMember);
     }
 
     @Override
-    public MemberResponse deleteProjectMember(Long projectId, Long memberId, Long userId) {
-        return null;
+    public void removeProjectMember(Long projectId, Long memberId, Long userId) {
+        Project project = getAccessibleProjectByIdAndUserId(projectId, userId);
+
+        if(!project.getOwner().getId().equals(userId))
+            throw new RuntimeException("Only project owner can update members role");
+
+        ProjectMemberId projectMemberId = new ProjectMemberId(projectId, memberId);
+
+        if(!projectMemberRepository.existsById(projectMemberId))
+            throw new RuntimeException("Member not found in the project");
+
+        projectMemberRepository.deleteById(projectMemberId);
+    }
+
+    /// Internal method to get project by id and userId
+    private Project getAccessibleProjectByIdAndUserId(Long projectId, Long userId) {
+        return projectRepository.findAccessibleProjectById(projectId, userId)
+                .orElseThrow();
     }
 }
